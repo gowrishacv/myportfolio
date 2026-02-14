c49fe26af93b6dae8aa72da922e3123575ca3468
Gowrisha C Vishwa Kumar <gowrish@outlook.com>
2026-02-14 20:09:15 +0100

    refactor: simplify build command in GitHub Actions and remove pre-production workflow

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
         <LanguageSwitcher />
       </div>
 
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route
